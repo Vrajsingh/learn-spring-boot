77440a35a6e05293da5b730dd5e90c0311b4ce05
Vishal Raj <vishalraj@Viveks-MacBook-Pro.local>
2024-06-21 01:11:17 +0530

    Day1 : Created first course controller api

--- src/main/java/com/in28minutes/springboot/learn_spring_boot/CourseController.java
+++ src/main/java/com/in28minutes/springboot/learn_spring_boot/CourseController.java
@@ -1,8 +1,20 @@
 package com.in28minutes.springboot.learn_spring_boot;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @RestController
 public class CourseController {
-    
+
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+        return Arrays.asList(
+            new Course(1, "Learn AWS", "in28minutes"),
+            new Course(2, "Learn DevOps", "in28minutes")
+        );
+    }
 }
